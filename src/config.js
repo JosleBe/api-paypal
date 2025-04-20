967b8681d39f352b222ab836b1a6b019f40eb118
Jose Leonardo Martinez Gomez <20223tn067@utez.edu.mx>
2025-04-20 11:21:32 -0600

    Dependencias

--- src/config.js
+++ src/config.js
@@ -5,11 +5,7 @@ config();
 export const PAYPAL_API_CLIENT = process.env.PAYPAL_API_CLIENT;
 export const PAYPAL_API_SECRET = process.env.PAYPAL_API_SECRET;
 export const PAYPAL_API = process.env.PAYPAL_API;
-
+export const HOST = process.env.HOST || "http://localhost:3002";
 
 // Server
 export const PORT = process.env.PORT || 3002;
-export const HOST =
-  process.env.NODE_ENV === "production"
-    ? process.env.HOST
-    : "http://localhost:" + PORT;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import path from "path";
-import { PORT } from "./config.js";
+import { PORT,HOST } from "./config.js";
 
 import paymentRoutes from "./routes/payment.routes.js";
 
@@ -16,5 +16,5 @@ app.use(paymentRoutes);
 app.use(express.static(path.resolve("./src/public")));
 
 app.listen(PORT);
-console.log(`Server on port http://localhost:${PORT}`);
+console.log(`Server on port ${HOST}:${PORT}`);
 console.log(`environment: ${process.env.NODE_ENV}`);
